939e588922009b537fa36046c38e6833794fd0ff
Sholom Aber <SholomAber@users.noreply.github.com>
2018-09-27 23:23:08 -0400

    return token and start of auth

--- app.js
+++ app.js
@@ -9,10 +9,34 @@ app.get('/api', (req, res) => {
     });
 });
 
-app.post('/api/posts', (req, res) => {
+app.post('/api/posts', verifyToken, (req, res) => {
     res.json({
         message: 'Post created...'
     });
 })
+
+app.post('/api/login', (req, res) => {
+
+    const user = {
+        id: 1,
+        username: 'Sholom',
+        email: 'sholom@gmail.com'
+    }
+
+    jwt.sign({user}, 'secretkey', (err, token) => {
+        res.json({
+            token
+        });
+    });
+});
+
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    if(typeof bearerHeader !== 'undefined') {
+
+    } else {
+        res.sendStatus(403);
+    }
+}
 
 app.listen(5000, () => console.log('Server started on port 5000'));
